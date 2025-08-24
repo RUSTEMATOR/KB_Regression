5644a42c1c0fa8687bb867950c16e138f5e303f4
Rustem Samoilenko <139997441+RUSTEMATOR@users.noreply.github.com>
2025-08-25 02:26:34 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,11 +51,6 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.kingbillycasino.com',
     headless: true,
-    proxy: {
-      server: 'proxy.geonode.io:9000',
-      username: 'geonode_Zr3aVjywHC-type-residential-country-ie-state-munster',
-      password: 'bebe29a2-c13b-4aa5-8c20-eb3dd10a8afd'
-    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     video: 'on-first-retry',
